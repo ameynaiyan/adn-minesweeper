41c0c4741c965ec49862a867a08ac956e4dedb82
Amey Naiyan <ameyd9@gmail.com>
2018-03-28 11:06:58 -0400

    Configured routing for the three views

--- angular-ui/src/app/app-routing.module.ts
+++ angular-ui/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GameSceneComponent } from './view/game-scene/game-scene.component';
+import { HighScoresComponent } from './view/high-scores/high-scores.component';
+import { MainMenuComponent } from  './view/main-menu/main-menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'play', component: GameSceneComponent },
+  { path: 'scores', component: HighScoresComponent },
+  { path: 'main', component: MainMenuComponent },
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
