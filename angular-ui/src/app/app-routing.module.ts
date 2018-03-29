cd5cf8241f4de91ef2bf4b9d061bf8049428c3fa
Amey Naiyan <ameyd9@gmail.com>
2018-03-29 11:44:18 -0400

    Added router configuration for new game config view

--- angular-ui/src/app/app-routing.module.ts
+++ angular-ui/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { GameSceneComponent } from './view/game-scene/game-scene.component';
 import { HighScoresComponent } from './view/high-scores/high-scores.component';
 import { MainMenuComponent } from  './view/main-menu/main-menu.component';
+import { NewGameConfigComponent } from './view/new-game-config/new-game-config.component';
 
 const routes: Routes = [
   { path: 'play', component: GameSceneComponent },
   { path: 'scores', component: HighScoresComponent },
   { path: 'main', component: MainMenuComponent },
+  { path: 'new', component: NewGameConfigComponent },
   { path: '', redirectTo: '/main', pathMatch: 'full' },
 ];
 
