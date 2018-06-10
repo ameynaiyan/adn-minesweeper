fd1ea12de2407df270c5ada0d81389eb23f74a51
Amey Naiyan <ameyd9@gmail.com>
2018-06-10 18:33:13 -0400

    Created new game config view component

--- adn-minesweeper-ui/src/app/app.module.ts
+++ adn-minesweeper-ui/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './view/main/main.component';
+import { NewComponent } from './view/new/new.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    NewComponent
   ],
   imports: [
     BrowserModule,
