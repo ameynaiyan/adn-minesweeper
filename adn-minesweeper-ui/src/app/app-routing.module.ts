b25f4ee4f5f2958a166a04c60ecea53fb9a71be9
Amey Naiyan <ameyd9@gmail.com>
2018-06-10 19:04:49 -0400

    Added Route link to all view components

--- adn-minesweeper-ui/src/app/app-routing.module.ts
+++ adn-minesweeper-ui/src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { MainComponent } from './view/main/main.component';
+import { NewComponent } from './view/new/new.component';
+import { SceneComponent } from './view/scene/scene.component';
+import { ScoresComponent } from './view/scores/scores.component';
+import { SummaryComponent } from './view/summary/summary.component';
+import { BackdropComponent } from './common/backdrop/backdrop.component';
+
+const routes: Routes = [
+	{
+		path: '', 
+		component: MainComponent
+	},
+	{
+		path: 'new', 
+		component: NewComponent
+	},
+	{
+		path: 'play', 
+		component: SceneComponent
+	},
+	{
+		path: 'scores', 
+		component: ScoresComponent
+	},
+	{
+		path: 'summary', 
+		component: SummaryComponent
+	},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
