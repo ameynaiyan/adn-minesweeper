3fc12adefe8535e62bd2cdc7c92167fe201ddd7d
Amey Naiyan <ameyd9@gmail.com>
2018-06-11 09:00:13 -0400

    Updated routes and links between views

--- adn-minesweeper-ui/src/app/view/scene/scene.component.ts
+++ adn-minesweeper-ui/src/app/view/scene/scene.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-scene',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SceneComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  goToMain() {
+  	this.router.navigateByUrl('/');
+  }
+  
+  restartGame() {
   }
 
 }
